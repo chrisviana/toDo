d635ca9f95a424765377c0dd182bb53978d8d88e
Christian Viana <viana2015christian@gmail.com>
2022-10-08 17:34:17 -0300

    Finalizado componente Tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,3 +1,28 @@
+import styles from "./Tasks.module.css";
+
 export function Tasks() {
-  return <section></section>;
+  return (
+    <section className={styles.tasks}>
+      <header className={styles.header}>
+        <div>
+          <p>Tarefas criadas</p>
+          <span>10</span>
+        </div>
+
+        <div>
+          <p className={styles.textPurple}>Concluídas</p>
+          <span>0 de 10</span>
+        </div>
+      </header>
+
+      <div className={styles.list}>
+        <section className={styles.empty}>
+          <div>
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
 }
